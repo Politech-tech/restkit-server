3ad60f40416cb6d34d48fc5addeb3fed9fd7db72
ido.shafrir <ido.shafrir@gmail.com>
2025-11-21 16:12:39 +0200

    add logger util aliases

--- src/restkit_server/__init__.py
+++ src/restkit_server/__init__.py
@@ -1,11 +1,14 @@
 """RestKit Server - A Flask-based REST server toolkit."""
 
 from .server_utils import SimpleServer, AdvancedServer, RestCodes, RestResponse
-
+from .logger import setup_logger, enter_exit_logger, LoggerWriter
 __version__ = "0.1.0"
 __all__ = [
     "SimpleServer",
     "AdvancedServer", 
     "RestCodes",
     "RestResponse",
+    "setup_logger",
+    "enter_exit_logger",
+    "LoggerWriter",
 ]
